e13812ae85b016ced30328dc036cbfcab22d325c
Lucas Martins <lucassantosm711@gmail.com>
2024-06-25 22:33:28 -0300

    Starting implements class Board and Piece

---
+++ aplication/Program.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import boardgame.Board;
+
+public class Program {
+    public static void main(String[] args) {
+        Board board = new Board(8, 8);
+
+        System.out.println("Hello World!");
+    }
+
+    
+}
